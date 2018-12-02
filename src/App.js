93d4e1a88dc3d603d5dbdcd0c6e62823eb6deb45
kakaly <reach.kkarthik@gmail.com>
2018-12-02 12:54:37 -0800

    Final

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      x: "",
+      y: "",
+      color: "#FFFFFF"
+    }
+    this.showCoords = this.showCoords.bind(this)
+    this.clearCoords = this.clearCoords.bind(this)
+    this.generateColor = this.generateColor.bind(this)
+  }
+
+  showCoords(event) {
+    var x = event.clientX
+    var y = event.clientY
+    this.setState({
+      x:event.clientX,
+      y:event.clientY,
+      color:this.generateColor(x, y)
+    })
+  }
+
+  clearCoords() {
+    this.setState({
+      x:"",
+      y:"",
+      color:"#FFFFFF"
+    })
+  }
+
+  generateColor(x, y) {
+    var xx = x.toString(16)
+    var yy = y.toString(16)
+    var zz = "00"
+    return ("#"+zz+xx.toString()+yy.toString())
+  }
+
   render() {
+    const {color} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{backgroundColor: color}}
+           className="div-element"
+           onMouseMove={this.showCoords}
+           onMouseOut={this.clearCoords}>
+           React Magic!!
       </div>
-    );
+    )
   }
 }
 
